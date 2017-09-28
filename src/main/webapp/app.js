bfbf3a17fcdef3a5a4c775f83f5b82089fbad42c
abhijithonline <abhijithonline@gmail.com>
2017-09-28 10:30:51 +0400

    Update with comments

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,30 +1,36 @@
 'use strict';
 var employeeApp = angular.module("employeeApp", []);
 
-
+//Initializes the Angular App and creates the factory methods
 employeeApp
 .factory('EmployeeDataOperations', ['$http', function ($http) {
   
       var urlBase = 'http://localhost:8080';
-      var EmployeeDataOperations = {};
-  
+      var EmployeeDataOperations = {}; //Factory
+    
+      //This returns all the employees
       EmployeeDataOperations.getEmployees = function () {
           return $http.get(urlBase+'/employees');
       };
   
+      //This adds the employee details from view
       EmployeeDataOperations.addEmployee = function (stud) {
           return $http.post(urlBase + '/employees', stud);
       };
 
+      //This updates the employee details based on the Id
       EmployeeDataOperations.updateEmployee = function (stud, $index) {
         return $http.put(urlBase + '/employees/' + stud[$index].ids , stud[$index]);
       };
 
+      //This remove the employee selected 
       EmployeeDataOperations.removeEmployee = function (stud, $index) {
         return $http.delete(urlBase + '/employees/' + stud[$index].ids , stud[$index]);
       };
 
+      //This searches the employee based on Name give by user
       EmployeeDataOperations.searchEmployees = function (searchName) {
+        alert('searchName'+searchName);
         return $http.get(urlBase + '/employees?firstName=' + searchName);
       };
 
@@ -34,15 +40,17 @@ employeeApp
 
   }]);
 
-
+ //Initialized the controller for the module and passes the factory
   employeeApp.controller('EmployeeController', function ($scope, EmployeeDataOperations) {
     $scope.status;
-    $scope.employees;
+    $scope.employees = [];
     $scope.selectedFile = null;  
     $scope.msg = "";  
 
+    //Lists all employees
     getEmployees();
 
+    //To get all employees
     function getEmployees() {
         EmployeeDataOperations.getEmployees()
             .success(function (studs) {
@@ -53,18 +61,25 @@ employeeApp
             });
     }
 
-    function searchEmployees() {
-        var searchName = $scope.name;
+    //To search employees based on Name
+    $scope.searchEmployees = function searchEmployees() {
+        var searchName = $scope.name;       
+       
         EmployeeDataOperations.searchEmployees(searchName)
-            .success(function () {
-                $scope.employees = studs._embedded.employees;
-                $scope.employees.push(searchName);
+            .success(function (studs) {                             
+
+               $scope.employees = [];
+               studs._embedded.employees.forEach(function (entry) {
+                   $scope.employees.push(entry);
+               });
+               $scope.$apply();
             })
             .error(function (error) {
-                $scope.status = 'Unable to load customer data: ' + error.message;
+                //$scope.status = 'Unable to load customer data: ' + error.message;
             });
     }
 
+    //To add employee
     $scope.addEmployee = function () {
 
         var stud = {
@@ -84,6 +99,7 @@ employeeApp
             });
     };
 
+    //To update selected employee
     $scope.updateEmployee = function ($index) {
                
         EmployeeDataOperations.updateEmployee($scope.employees, $index)
@@ -96,6 +112,7 @@ employeeApp
             });
     };
 
+    //To remove employee
     $scope.removeEmployee = function ($index) {
         
             EmployeeDataOperations.removeEmployee($scope.employees, $index)
@@ -108,12 +125,14 @@ employeeApp
         });
     };
     
+    //To choose file
     $scope.loadFile = function (files) {          
         $scope.$apply(function () {          
             $scope.selectedFile = files[0];          
         });          
     };
 
+    //To process file and convert to JSON
     $scope.handleFile = function () {  
         
             var file = $scope.selectedFile;      
@@ -131,17 +150,24 @@ employeeApp
     
                     var dataObjects = XLSX.utils.sheet_to_json(workbook.Sheets[first_sheet_name]);                         
     
-                    if (dataObjects.length > 0) {   
-                        alert('dataObjectsdsds'+dataObjects[0].email);   
+                    if (dataObjects.length > 0) {                           
                        
-                        $scope.employees = dataObjects;                                                
+                        alert('Uploaded: '+dataObjects[0].firstName);                         
+                        EmployeeDataOperations.addEmployee(JSON.stringify(dataObjects))
+                        .success(function () {
+                            $scope.status = 'Inserted Employee! Refreshing Employee list.';
+                            $scope.employees.push(getEmployees());
+                        }).
+                        error(function (error) {
+                            $scope.status = 'Unable to insert Employee: ' + error.message;
+                        });
     
                     } else {                         
                     }  
     
                 };  
     
-                //reader.readAsBinaryString(file);  
+                reader.readAsBinaryString(file);  
             }  
         }; 
    
